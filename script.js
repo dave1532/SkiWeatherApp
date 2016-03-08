70da581b6e6b1c78d82cb1c3d00d989aac2fd7b7
dave1532 <davidrutkoske@gmail.com>
2016-03-08 09:17:23 -0800

    workOnForms

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-
-
 $(function () {
-        $('#datetimepicker6').datetimepicker();
-        $('#datetimepicker7').datetimepicker({
-            useCurrent: false //Important! See issue #1075
+            $('#datetimepicker1').datetimepicker();
         });
-        $("#datetimepicker6").on("dp.change", function (e) {
-            $('#datetimepicker7').data("DateTimePicker").minDate(e.date);
-        });
-        $("#datetimepicker7").on("dp.change", function (e) {
-            $('#datetimepicker6').data("DateTimePicker").maxDate(e.date);
-        });
-    });
