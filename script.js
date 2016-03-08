fcfb991db0d99201cac17030bde92406c25f047b
dave1532 <davidrutkoske@gmail.com>
2016-03-08 13:26:16 -0800

    changeToHikes

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
-$(function () {
-            $('#datetimepicker1').datetimepicker();
-        });
+$.ajax({
+  url:'http://api.openweathermap.org/data/2.5/weather?zip=98105,us&units=imperial&appid=de5c2116380f8c980ccb5a4a25b4a2f5',
+  method: "GET",
+  success: function(data){
+    alert(data);
+  })
+}
