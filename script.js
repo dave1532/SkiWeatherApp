0e32e0bc95de8eaae75456fb02a8f44cc4700d0c
dave1532 <davidrutkoske@gmail.com>
2016-03-08 15:28:28 -0800

    gotPhoto

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-$.ajax({
-  url:'http://api.openweathermap.org/data/2.5/weather?zip=98105,us&units=imperial&appid=de5c2116380f8c980ccb5a4a25b4a2f5',
-  method: "GET",
-  success: function(data){
-    alert(data);
-  })
-}
+
