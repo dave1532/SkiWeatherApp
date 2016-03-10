4372c06afd8807570d58cf511b4c63885fa19aa0
dave1532 <davidrutkoske@gmail.com>
2016-03-09 16:50:28 -0800

    workOnformjs

--- script.js
+++ script.js
@@ -1,22 +1,47 @@
-$.ajax({
-  url:"http://api.openweathermap.org/data/2.5/weather?lat=47.61&lon=-122.33&units=imperial&appid=" + weatherAppId,
-  method: "GET",
-  success: function(data){
-    var ourData = {
-      temp: data.main.temp,
-      windSpeed: data.wind.speed,
-      main: data.main,
-      weather: data.weather.description,
-    }
-
-    var $cast = $('<div id="forecast">' + data.main + '</div>')
-
-    $(".container").append($cast)
-
-
+var locations = {
+  greenLake: {
+    lat: 47.61,
+    long: -122.33,
   }
-});
-// $("#Green",".BtnSub").on('click', function(){
-//   console.log($.on());
-// }
-// function On($.ajax) {
+  discovery:{
+    lat: 47.661976,
+    long: -122.435705,
+  }
+  carkeek:{
+    lat: 47.640139,
+    long: -122.293780,
+  }
+  interlaken:{
+    lat: 47.642062,
+    long: -122.317115,
+  }
+ lincolnPark:{
+    lat: 47.531497,
+    long: -122.392876,
+  }
+}
+
+function go(e) {
+  e.preventDefault();
+  var selection = document.George.example.options[document.George.example.selectedIndex].value;
+  fetchWeatherData(selection);
+}
+
+$('.formSubmit').click(go);
+
+function fetchWeatherData(selection) {
+  $.ajax({
+    url:'http://api.openweathermap.org/data/2.5/weather?lat=' + locations[selection].lat + '&lon=' + locations[selection].long + '&units=imperial&appid=' + weatherAppId,
+    method: "GET",
+    success: function(data){
+      var ourData = {
+        temp: data.main.temp,
+        windSpeed: data.wind.speed,
+
+      }
+      var $cast = $('<div id="forecast">' + data.main.temp + '</div>')
+
+      $(".container").append($cast)
+    }
+  });
+}
